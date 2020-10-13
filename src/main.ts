207ec6b36a1ba4414e09b45d256ec4308cc1bd93
autopp <autopp.inc@gmail.com>
2020-10-14 08:20:54 +0900

    refactor: delete vuex code

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import Vue from "vue"
 import VueClipboard from "vue-clipboard2"
 import App from "./App.vue"
 import router from "./router"
-import store from "./store"
 
 Vue.config.productionTip = false
 Vue.use(VueClipboard)
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app")
